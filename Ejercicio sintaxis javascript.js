ff93e13fb18aca056ba91b801b1b1ecaf35242d6
Julio Uva <34964918+juliocuva@users.noreply.github.com>
2022-11-25 19:42:19 -0500

    Add files via upload

---
+++ Ejercicio sintaxis javascript.js
@@ -0,0 +1,20 @@
+const nombre = ("Julio Uva");
+const edad = ("tu edad", 50);
+const desarrollador = ("¿Eres desarrollador?", true);
+const fecha = new Date ("november 19 1971");
+const lista =[nombre, edad, desarrollador,fecha]
+
+console.log (lista)
+
+const libro = {
+    titulo: "Angeles y Demonios",
+    autor: "Dan Brown",
+    anyo: 2019,
+    URL: "https://www.amazon.com/-/es/Dan-Brown/dp/8492516518",
+}
+
+console.log (libro.titulo)
+console.log (libro.URL)
+
+const ahora = new Date()
+console.log(ahora)
