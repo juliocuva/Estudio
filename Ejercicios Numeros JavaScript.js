97ccd8f6a9271740126c1f43deb3d0fe2e89741d
Julio Uva <34964918+juliocuva@users.noreply.github.com>
2022-12-14 10:24:32 -0500

    Add files via upload

---
+++ Ejercicios Numeros JavaScript.js
@@ -0,0 +1,18 @@
+let altura = 185;
+console.log ("mi altura es:" , altura , "cms");
+
+let metros = 1.85;
+console.log ("mi altura es:" , metros , "cms");
+
+let peso = 86;
+console.log ("mi peso es:" , peso , "Kg");
+
+let peso_round = 86.25436;
+console.log ("mi peso real es", Math.round ((peso_round)*100)/100 , "kg");
+
+let altura_round = 1.82578;
+console.log ("mi peso real es", altura_round.toFixed(2), "mt");
+
+let max_valor_JS = Number.MAX_VALUE;
+let number_esp = 1;
+console.log ((max_valor_JS.toPrecision(1)) + number_esp);
